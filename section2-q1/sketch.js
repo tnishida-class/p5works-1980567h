092d375dd31af5bc0ba759be4c5ac57cb4f99cd5
1980567h <1980567h@stu.kobe-u.ac.jp>
2020-10-31 01:27:10 +0900

    Change section2-1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+noFill();
+stroke(0,0,255);
+for(let i = 0; i<5; i++){
+  let x=(i + 1)*10;
+  ellipse(50,50,x)
+}
+  noFill();
+  stroke(255,0,0);
+  for(let s = 0; s<5; s++){
+    let x=(s + 6)*10;
+    ellipse(50,50,x)
+}
+      // BLANK[1]
 }
